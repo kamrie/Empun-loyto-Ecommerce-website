08440c38e8fbfaedd54f1227c94d6ce948d47121
kamrie <ifedikeze@gmail.com>
2025-02-08 17:50:11 +0100

    Added SQL export file in database folder

---
+++ database/mirabella_project.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 08, 2025 at 05:40 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mirabella_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `order_cost` decimal(6,2) NOT NULL,
+  `order_status` varchar(100) NOT NULL DEFAULT 'on_hold',
+  `user_id` int(11) NOT NULL,
+  `user_phone` int(11) NOT NULL,
+  `user_city` varchar(255) NOT NULL,
+  `user_address` varchar(255) NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_category` varchar(100) NOT NULL,
+  `product_description` varchar(255) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `product_image2` varchar(255) NOT NULL,
+  `product_image3` varchar(255) NOT NULL,
+  `product_image4` varchar(255) NOT NULL,
+  `product_price` decimal(6,2) NOT NULL,
+  `product_special_offer` int(2) NOT NULL,
+  `product_color` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `product_category`, `product_description`, `product_image`, `product_image2`, `product_image3`, `product_image4`, `product_price`, `product_special_offer`, `product_color`) VALUES
+(1, 'Crystal Beaded Bracelet', 'Bracelets', 'Elegant crystal bracelet with natural stone beads.', 'featured 1.webp', 'featured 1.webp', 'featured 1.webp', 'featured 1.webp', 155.00, 0, 'goldenrod'),
+(2, 'Gold-Plated Earrings', 'Earrings', 'Vintage-style gold-plated earrings with gemstone inlays.', 'earring 1.webp', 'earring 1.webp', 'earring 1.webp', 'earring 1.webp', 164.00, 0, 'Gold'),
+(3, 'Butterfly Pendant Necklace', 'Necklaces', 'Delicate butterfly pendant necklace with a gold chain.', 'necklace 1.webp', 'necklace 1.webp', 'necklace 1.webp', 'necklace 1.webp', 299.50, 0, 'Gold'),
+(4, 'Heart Shape Stainless Steel Bangle', 'Bangle', 'Trendy heart shape bangle made from stainless steel.', 'bangle 1.webp', 'bangle 1.webp', 'bangle 1.webp', 'bangle 1.webp', 403.43, 0, 'Silver'),
+(5, 'Luxury Crystal Ring', 'Rings', 'Beautiful crystal-embedded ring for special occasions.', 'ring.webp', 'ring.webp', 'ring.webp', 'ring.webp', 35.00, 0, 'Rose Gold');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `UX_Constraint` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
